30bcfe0c72e9233ad3aae411d4229226e8d97746
sungjune <sjk5470@gmail.com>
2023-08-01 11:30:36 +0900

    REST API 등록 GET POST 컨트롤러 생성

---
+++ src/main/java/com/study/base/boot/aggregations/v1/order/presentation/OrderController.java
@@ -0,0 +1,23 @@
+package com.study.base.boot.aggregations.v1.order.presentation;
+
+import com.study.base.boot.config.annotations.Get;
+import com.study.base.boot.config.annotations.Post;
+import com.study.base.boot.config.annotations.RestApi;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@RestApi("/v1/orders")
+public class OrderController {
+
+    @Get
+    public List<String> getOrders() {
+        return List.of("A","B","C");
+    }
+
+    @Post
+    public List<String> createOrders(@RequestBody List<String> orderList){
+        return orderList;
+    }
+
+}
